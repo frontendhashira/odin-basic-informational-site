68d405c884f3fd500c01e947bf21e0cd2b36c88f
ridwan <ridwanyinusa15@gmail.com>
2025-09-22 23:51:42 +0100

    initial commit 🖤

---
+++ index.js
@@ -0,0 +1,39 @@
+import fs from "node:fs/promises";
+import http from "node:http";
+
+const PORT = 5000;
+
+const routes = new Map([
+	["home", { file: "./index.html", code: 200 }],
+	["about", { file: "./about.html", code: 200 }],
+	["contact", { file: "./contact-me.html", code: 200 }],
+	["404", { file: "./404.html", code: 404 }],
+]);
+
+const getRoute = (path) => {
+	switch (path) {
+		case "/":
+			return routes.get("home");
+		case "/about":
+			return routes.get("about");
+		case "/contact":
+			return routes.get("contact");
+
+		default:
+			return routes.get("404");
+	}
+};
+
+const getFileContent = async (pathUrl) => {
+	const response = getRoute(pathUrl);
+	const data = await fs.readFile(response.file, { encoding: "utf-8" });
+	return { data, statusCode: response.code };
+};
+
+const server = http.createServer(async (req, res) => {
+	const { data, statusCode } = await getFileContent(req.url);
+	res.writeHead(statusCode, { "Content-Type": "text/html" });
+	res.end(data);
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
