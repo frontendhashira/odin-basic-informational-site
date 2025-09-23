ede47acb016a56fb0ae6053f380e16ba3ef49db6
ridwan <ridwanyinusa15@gmail.com>
2025-09-23 14:32:04 +0100

    feat: initialize project with express and setup basic routing
    - Added .gitignore to exclude node_modules
    - Removed old 404.html and created a new one with improved styling
    - Refactored index.js to use express for routing and static file serving
    - Created public directory with index.html, about.html, and contact-me.html
    - Updated package.json to include express as a dependency
    - Added pnpm-lock.yaml for dependency management

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
-import fs from "node:fs/promises";
-import http from "node:http";
+import path from "node:path";
+import url from "node:url";
+import express from "express";
+
+const app = express();
 
 const PORT = 5000;
 
-const routes = new Map([
-	["home", { file: "./index.html", code: 200 }],
-	["about", { file: "./about.html", code: 200 }],
-	["contact", { file: "./contact-me.html", code: 200 }],
-	["404", { file: "./404.html", code: 404 }],
-]);
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const getRoute = (path) => {
-	switch (path) {
-		case "/":
-			return routes.get("home");
-		case "/about":
-			return routes.get("about");
-		case "/contact":
-			return routes.get("contact");
+// setup static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-		default:
-			return routes.get("404");
-	}
-};
-
-const getFileContent = async (pathUrl) => {
-	const response = getRoute(pathUrl);
-	const data = await fs.readFile(response.file, { encoding: "utf-8" });
-	return { data, statusCode: response.code };
-};
-
-const server = http.createServer(async (req, res) => {
-	try {
-		const { data, statusCode } = await getFileContent(req.url);
-		res.writeHead(statusCode, { "Content-Type": "text/html" });
-		res.end(data);
-	} catch (error) {
-		console.error(error);
-		res.writeHead(500, { "Content-Type": "text/plain" });
-		res.end("Internal Server Error");
-	}
+app.use((req, res) => {
+	res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
 });
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
